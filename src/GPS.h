c1aa368286173c87e81285649eba32ba9cf85fc5
David Gelkin <david@davidgelkin.com>
2019-10-12 22:34:33 +0300

    initial GPS testing program. Added GPS datasheets and guides. Added NMEA codes explanations

---
+++ src/GPS.h
@@ -0,0 +1,5 @@
+#ifndef _GPS_H
+#define _GPS_H
+
+
+#endif // _GPS_H
---
+++ test/test_GPS.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "GPS.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_GPS_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement GPS");
+}
