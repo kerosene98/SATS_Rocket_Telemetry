a3349de3e4d2ae9126788aa123ad5877c948a239
David Gelkin <david@davidgelkin.com>
2019-10-12 23:48:37 +0300

    updated to new ide

--- src/main/main.ino
+++ src/main/main.ino
@@ -15,46 +15,31 @@ uint32_t timer = millis();
 
 void setup()
 {
-  //while (!Serial);  // uncomment to have the sketch wait until Serial is ready
 
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
   Serial.begin(115200);
   Serial.println("NMEA GPGGA ONLY!");
 
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   GPS.begin(9600);
-  // ALL data
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_GGAONLY);
- 
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
-
-  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);     // 1 Hz update rate
   GPS.sendCommand(PGCMD_ANTENNA);
 
   delay(1000);
 
-  // Ask for firmware version and release
   GPSSerial.println(PMTK_Q_RELEASE);
 }
 
-void loop() // run over and over again
+
+/* Main Loop */
+void loop()
 {
-  // read data from the GPS in the 'main loop'
+ 
   char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
+ 
   if (GPSECHO)
     if (c) Serial.print(c);
   // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
     Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
     if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
       return; // we can fail to parse a sentence in which case we should just wait for another
